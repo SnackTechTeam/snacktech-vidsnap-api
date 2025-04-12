51c4ae69c86dbeee0cd7332bac0903a2fb3b57b7
Dayvid Ribeiro Correia <dayvid@sorteonline.com.br>
2025-04-12 20:32:18 -0300

    implementando publicação de mensagem de notificação de usuário após finalização de processamento do vídeo

---
+++ src/Core/Vidsnap.Domain/Ports/Outbound/IVideoPublisher.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using Vidsnap.Domain.Entities;
+
+namespace Vidsnap.Domain.Ports.Outbound
+{
+    public interface IVideoPublisher
+    {
+        Task PublicarProcessamentoFinalizadoAsync(Video video, CancellationToken cancellationToken = default);
+    }
+}
